511d89f48a9d9c163f380ff86d77f345d6513deb
yusuf-mohammad <yusuf.mohammad@gmail.com>
2013-07-01 12:53:59 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hgy::Application.config.secret_token = 'fc32fa08ca8424ee66485f6ab6fe30209a14f7dd76451f179c7039fe6705d4324d0c69939f495b35cb2af0dc2b5d1eeea65a3aa31880c7809f97c346605ac8bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hgy::Application.config.session_store :cookie_store, key: '_hgy_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hgy::Application.config.session_store :active_record_store
